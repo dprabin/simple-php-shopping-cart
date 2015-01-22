e19f4e2ec509bfa793615e277934f4e21f7842a5
dprabin <dprabin@yahoo.com>
2015-01-22 23:23:16 +0545

    Some modifications to products view

--- application/views/products.php
+++ application/views/products.php
@@ -4,7 +4,15 @@
 		<div class="food-price">Rs.<?php echo $product->price; ?></div>
 		<div class="food-image"><a href="<?php echo base_url(); ?>products/details/<?php echo $product->id; ?>"><img class="food-image-img" src="<?php echo base_url(); ?>assets/images/products/<?php echo $product->image; ?>" /></a></div>
 		<div class="food-title"><?php echo $product->title; ?></div>
-		<div class="food-add"><button class="btn btn-primary" type="submit">Add To Cart</button></div>
+		<div class="food-add">
+			<form method="post" action="<php echo base_url(); ?>cart/add">
+				QTY: <input type="text" class="qty" name="qty" value="1" /> 
+				<input type="hidden" name="item_number" value="<?php echo $product->id; ?>" />
+				<input type="hidden" name="price" value="<?php echo $product->price; ?>" />
+				<input type="hidden" name="title" value="<?php echo $product->title; ?>" />
+				<button class="btn btn-primary" type="submit">Add To Cart</button>
+			</form>
+		</div>
 	</div>
 <?php endforeach; ?>
 </div><!-- /Food items -->
