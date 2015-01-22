f8ea7ab2aba4b32dc676f043e1d42325eb0f6b62
dprabin <dprabin@yahoo.com>
2015-01-23 00:01:39 +0545

    Changed Layout of detais view

--- application/views/details.php
+++ application/views/details.php
@@ -1,13 +1,8 @@
 <div class="row details">
-	<div class="col-md-4">
-		<div class="food-image-details"><img class="food-image-img" src="<?php echo base_url(); ?>assets/images/products/<?php echo $product->image; ?>" /></div>
-	</div>
+	<div class="col-md-12"><h3><?php echo $product->title; ?></h3></div>
 	<div class="col-md-8">
-		<h3><?php echo $product->title; ?></h3>
+		<div class="food-image-details"><img class="food-image-img" src="<?php echo base_url(); ?>assets/images/products/<?php echo $product->image; ?>" /></div>
 		<div class="details-price">Price: Rs.<?php echo $product->price; ?> per <?php echo $product->unit; ?></div>
-		<div class="details-description">
-		  <p><?php echo $product->description; ?></p>
-		</div>
 		<div class="details-buy">
 			<form method="post" action="<php echo base_url(); ?>cart/add">
 				QTY: <input type="text" class="qty" name="qty" value="1" /> 
@@ -16,6 +11,14 @@
 				<input type="hidden" name="title" value="<?php echo $product->title; ?>" />
 				<button class="btn btn-primary" type="submit">Add To Cart</button>
 			</form>
+		</div>
+		<div class="details-description"><p><?php echo $product->description; ?></p></div>
+	</div>
+	<div class="col-md-4">
+
+		<div class="details-description">
+		  <h4>Nutritional value</h4>
+		  <p><?php echo $product->nutritional_value; ?></p>
 		</div>
 	</div>
 </div>
