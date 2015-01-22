e435c30c0846706c932c175d815452fd248b9a2c
dprabin <dprabin@yahoo.com>
2015-01-23 02:29:52 +0545

    added method to return categories and popular products

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -20,6 +20,27 @@ class Product_model extends CI_Model{
 		$query = $this->db->get();
 		return $query->row();
 	}
+
+	//Get Categories
+	public function get_categories(){
+		$this->db->select('*');
+		$this->db->from('categories');
+		$query = $this->db->get();
+		return $query->result();
+	}
+
+	//Get Popular Product
+	public function get_popular(){
+		$this->db->select('p.title,p.id, count(o.product_id) as total');
+		$this->db->from('orders as o');
+		$this->db->join('products as p','o.product_id = p.id','inner');
+		$this->db->group_by('o.product_id');
+		$this->db->order_by('total','desc');
+		//$this->db->limit('5');
+		$query = $this->db->get();
+		return $query->result();
+	}
+
 }
 
 ?>
