25a6935377a7aef4c3bfd584fa31072659f53f78
dprabin <dprabin@yahoo.com>
2015-01-22 23:21:35 +0545

    added controller function to handle individual product and display product details

--- application/controllers/products.php
+++ application/controllers/products.php
@@ -6,8 +6,18 @@ class Products extends CI_Controller{
 
 
     	//Load View
-        //Defint the main content as products
+        //Define the main content area as products view (product.php)
         $data['main_content'] = 'products';
+        //load product view
+        $this->load->view('layouts/main',$data);
+    }
+
+    public function details($id){
+    	//Get Product Details
+    	$data['product'] = $this->Product_model->get_product_details($id);
+
+    	//Define the main content as details view
+        $data['main_content'] = 'details';
         //load product view
         $this->load->view('layouts/main',$data);
     }
