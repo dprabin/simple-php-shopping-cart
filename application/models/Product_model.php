e6cc0c62e843fb057c941ab075bb3f448c361046
dprabin <dprabin@yahoo.com>
2015-01-22 15:57:45 +0545

    Read all items from products table

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -8,7 +8,7 @@ class Product_model extends CI_Model{
 	public function get_products(){
 		$this->db->select('*');
 		$this->db->from('products');
-		$query->$this->db->get();
+		$query = $this->db->get();
 		return $query->result();
 	}
 }
