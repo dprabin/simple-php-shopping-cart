57453316e083c47fb51718fdaa07c415baf7a023
dprabin <dprabin@yahoo.com>
2015-01-23 01:08:01 +0545

    changed layout of view

--- application/views/details.php
+++ application/views/details.php
@@ -1,11 +1,11 @@
 <div class="row details">
-	<div class="col-md-12"><h3><?php echo $product->title; ?></h3></div>
+	<div class="col-md-12"><div class="details-food-title"><h3><?php echo $product->title; ?></h3></div></div>
 	<div class="col-md-8">
 		<div class="food-image-details"><img class="food-image-img" src="<?php echo base_url(); ?>assets/images/products/<?php echo $product->image; ?>" /></div>
-		<div class="details-price">Price: Rs.<?php echo $product->price; ?> per <?php echo $product->unit; ?></div>
+		<div class="details-food-price">Price: Rs.<?php echo $product->price; ?> per <?php echo $product->unit; ?></div>
 		<div class="details-buy">
 			<form method="post" action="<php echo base_url(); ?>cart/add">
-				QTY: <input type="text" class="qty" name="qty" value="1" /> 
+				Quantity: <input type="text" class="qty" name="qty" value="1" /> 
 				<input type="hidden" name="item_number" value="<?php echo $product->id; ?>" />
 				<input type="hidden" name="price" value="<?php echo $product->price; ?>" />
 				<input type="hidden" name="title" value="<?php echo $product->title; ?>" />
