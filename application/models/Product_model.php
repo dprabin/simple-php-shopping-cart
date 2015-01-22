bcff89f2ed7f4afd7e1633b211fb7e06fb4266f1
dprabin <dprabin@yahoo.com>
2015-01-22 15:38:33 +0545

    get_products() method defined

---
+++ application/models/Product_model.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* 
+*/
+class Product_model extends CI_Model{
+
+	//Get all products
+	public function get_products(){
+		$this->db->select('*');
+		$this->db->from('products');
+		$query->$this->db->get();
+		return $query->result();
+	}
+}
+
+?>
