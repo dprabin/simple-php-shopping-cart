35744b1ee695a979d0691b0aa1dea659e9d458b2
dprabin <dprabin@yahoo.com>
2015-01-22 15:33:29 +0545

    Get products from products model

--- application/controllers/products.php
+++ application/controllers/products.php
@@ -1,10 +1,14 @@
 <?php 
 class Products extends CI_Controller{
     public function index(){
+    	//Get All Products
+    	$data['products'] = $this->Product_model->get_products();
+
+
+    	//Load View
         //Defint the main content as products
         $data['main_content'] = 'products';
         //load product view
-        
         $this->load->view('layouts/main',$data);
     }
 }
