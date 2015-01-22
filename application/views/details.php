290839c984136c1dcb007074ccd9d089214164e1
dprabin <dprabin@yahoo.com>
2015-01-22 23:20:44 +0545

    product details view

---
+++ application/views/details.php
@@ -0,0 +1,17 @@
+<div class="row details">
+	<div class="col-md-4">
+		<div class="food-image"><img class="food-image-img" src="images/almond1.jpg" /></div>
+	</div>
+	<div class="col-md-8">
+		<h3>Crunchy and Fresh Almonds</h3>
+		<div class="details-price">Price: Rs.950</div>
+		<div class="details-description">
+		  <p>Almonds: The almond is a species of tree native to the Middle East and South Asia. "Almond" is also the name of the edible and widely cultivated seed of this tree. Within the genus Prunus, it is classified with the peach in the subgenus Amygdalus, distinguished from the other subgenera by the corrugated shell (endocarp) surrounding the seed.</p>
+		</div>
+		<div class="details-buy">
+		  <form>
+		    <button type="submit" name="buy_submit" class="btn btn-primary">Add to Cart</button>
+		  </form>
+		</div>
+	</div>
+</div>
