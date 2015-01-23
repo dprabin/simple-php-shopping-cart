94b64a72474c5e1a282f02d4b469ddd74bc7a0fa
dprabin <dprabin@yahoo.com>
2015-01-23 14:03:26 +0545

    Add function in controller

--- application/controllers/cart.php
+++ application/controllers/cart.php
@@ -2,10 +2,37 @@
 /**
 * 
 */
-class ClassName extends CI_Controller{
+class Cart extends CI_Controller{
 	
 	public $paypal_data = '';
-	
+	public $tax;
+	public $shippint;
+	public $total = 0;
+	public $grand_total;
+
+	//Cart index
+	public function index(){
+		//Load View
+		$data['main_content'] = 'cart';
+		$this->load->view('layouts/main', $data);
+	}
+
+	//Add to cart
+	public function add(){
+		//Items Data
+		$data = array(
+			'id'=> $this->input->post('item_number'),
+			'qty' =>$this->input->post('qty'),
+			'price'=> $this->input->post('price'),
+			'name'=> $this->input->post('title'));
+
+		//to support unicode title as suggested in stackoverflow
+		$this->cart->product_name_rules = '\d\D';
+		//Insert into cart
+		$this->cart->insert($data);
+		//print_r($data);
+		redirect('products');
+	}
 }
 
 ?>
