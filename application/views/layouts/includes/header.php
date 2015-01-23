52b7765dc05f791bf4c529f38e9027e4f97c06c2
dprabin <dprabin@yahoo.com>
2015-01-23 16:40:57 +0545

    added method and action ot users/login

--- application/views/layouts/includes/header.php
+++ application/views/layouts/includes/header.php
@@ -36,9 +36,9 @@
     <div id="navbar" class="collapse navbar-collapse">
       <ul class="nav navbar-nav">
         <li class="active"><a href="<?php echo base_url(); ?>">Home</a></li>
-        <li><a href="<?php echo base_url(); ?>user/register">Create Account</a></li>
+        <li><a href="<?php echo base_url(); ?>users/register">Create Account</a></li>
       </ul>
-      <form class="navbar-form navbar-right">
+      <form method="post" action="<?php echo base_url(); ?>user/login" class="navbar-form navbar-right">
         <div class="form-group">
           <input name="username" type="text" class="form-control" placeholder="Username">
         </div>
