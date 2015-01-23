84b15aad1138cbe818e9c5cac00fb97cddffcdf3
dprabin <dprabin@yahoo.com>
2015-01-23 16:51:21 +0545

    added login method

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -18,9 +18,41 @@ class Users extends CI_Controller{
 				redirect('products');
 			}
 		}
-
-		//Show View
-		
 	}
+
+	//Login user 
+	public function login(){
+		//Form Validation
+		$this->form_validation->set_rules('username','Username', 'trim|required|max_length[20]|min_length[3]');
+		$this->form_validation->set_rules('password','Password', 'trim|required|max_length[50]|min_length[4]');
+	
+
+		$username = $this->input->post('username'); //$_POST['username'];
+		$password = md5($this->input->post('password'));
+
+		$user_id = $this->User_model->login($username,$password);
+
+		//Validate User
+		if($user_id){
+			//Create array of user data
+			$data = array(
+				'user_id' => $user_id,
+				'username' => $username,
+				'logged_in' => true );
+
+			//Set session data
+			$this->session->set_userdata($data);
+
+			//Set Flash message, that is stored in sessions and can be used in another controller or view
+			$this->session->set_flashdata('pass_login','You are logged in');
+			redirect('products');
+		} else {
+			//set error
+			$this->session->set_flashdata('fail_login', 'Sorry, the login information was not correct');
+			redirect('products');
+		}
+	}
+
+
 }
 ?>
