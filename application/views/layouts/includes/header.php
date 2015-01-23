c3907ad13993a94e5f5883d06f617b973d87547e
dprabin <dprabin@yahoo.com>
2015-01-23 17:25:07 +0545

    Display username when logged in, otherwiwse display register

--- application/views/layouts/includes/header.php
+++ application/views/layouts/includes/header.php
@@ -36,10 +36,10 @@
     <div id="navbar" class="collapse navbar-collapse">
       <ul class="nav navbar-nav">
         <li class="active"><a href="<?php echo base_url(); ?>">Home</a></li>
-        <?php if(!$this->session->user_data('logged_in')) : ?>
+        <?php if(!$this->session->userdata('logged_in')) : ?>
           <li><a href="<?php echo base_url(); ?>users/register">Create Account</a></li>
         <?php else : ?>
-          <li>Logged in as <?php echo $this->session->user_data('username'); ?></li>
+          <li><a href="#">Logged in as <?php echo $this->session->userdata('username'); ?></a></li>
         <?php endif; ?>
       </ul>
       <?php if(!$this->session->userdata('logged_in')) : ?>
