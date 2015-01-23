4f694778d85e1288e59d400c2b6ee1fb5146d78f
dprabin <dprabin@yahoo.com>
2015-01-23 15:57:57 +0545

    added form_validation library and url helper

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database','cart');
+$autoload['libraries'] = array('database','cart','form_validation');
 
 
 /*
