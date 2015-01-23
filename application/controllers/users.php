fe9def7f74e8a5adcd9ddc7d38308bff30917435
dprabin <dprabin@yahoo.com>
2015-01-23 16:07:17 +0545

    Form validation first name

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -1,10 +1,17 @@
 <?php
 class Users extends CI_Controller{
 	public function register(){
+		//Validation Rules
+		$this->form_validation->set_rules('first_name','First Name', 'trim|required|max_length[20]|min_length[2]');
+		if($this->form_validation->run() == FALSE){
+			$data['main_content'] = 'register';
+			$this->load->view('layouts/main',$data);
+		} else {
+			$this->load->view('formsuccess');
+		}
 
 		//Show View
-		$data['main_content'] = 'register';
-		$this->load->view('layouts/main',$data);
+		
 	}
 }
 ?>
