9970befe81d4858f37ab413eaac21c56abcda68b
dprabin <dprabin@yahoo.com>
2015-01-24 11:58:58 +0545

    added method to find user ip address

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -38,6 +38,29 @@ class User_model extends CI_Model{
 		//$this->db->where('id',$result->row(0)->id);
 		return false;
 	}
+
+	//Find ip address of User
+	public function find_user_ip(){
+		if ($_SERVER['HTTP_CLIENT_IP']){
+			$ip = $_SERVER['HTTP_CLIENT_IP'];
+		} else if($_SERVER['HTTP_X_FORWARDED_FOR']){
+			$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+		} else if($_SERVER['HTTP_X_FORWARDED']){
+			$ip = $_SERVER['HTTP_X_FORWARDED'];
+		} else if($_SERVER['HTTP_FORWARDED_FOR']){
+			$ip = $_SERVER['HTTP_FORWARDED_FOR'];
+		} else if($_SERVER['HTTP_FORWARDED']){
+			$ip = $_SERVER['HTTP_FORWARDED'];
+		} else if($_SERVER['REMOTE_ADDR']){
+			$ip = $_SERVER['REMOTE_ADDR'];
+		} else {
+			$ip = 'UNKNOWN';
+		}
+
+		return $ip;
+	}
+
+	//Find user Geolocation
 }
 
 ?>
