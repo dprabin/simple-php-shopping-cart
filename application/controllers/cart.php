5a11586be9c679b52a9b1798e5da2c4aff7849c6
dprabin <dprabin@yahoo.com>
2015-01-25 02:01:55 +0545

    Added process method to process order in cart

--- application/controllers/cart.php
+++ application/controllers/cart.php
@@ -3,7 +3,7 @@ class Cart extends CI_Controller{
 	
 	public $paypal_data = '';
 	public $tax;
-	public $shippint;
+	public $shipping;
 	public $total = 0;
 	public $grand_total;
 
@@ -43,20 +43,59 @@ class Cart extends CI_Controller{
 
 	//Process a cart
 	public function process(){
+		if($_POST) {
+			foreach ($this->input->post('item_name') as $key => $value) {
+				$item_id = $this->input->post('item_code')[$key];
+				$product = $this->Product_model->get_product_details('item_id');
+
+				//Paypal data
+				$this->paypal_data .= '&L_PAYMENTREQUEST_0_NAME'.$key.'='.urlencode($product->title);
+				$this->paypal_data .= '&L_PAYMENTREQUEST_0_NUMBER'.$key.'='.urlencode($item_id);
+				$this->paypal_data .= '&L_PAYMENTREQUEST_0_AMT'.$key.'='.urlencode($product->price);
+				$this->paypal_data .= '&L_PAYMENTREQUEST_0_QTY'.$key.'='.urlencode($this->input->post('item_qty')[$key]);
+
+				//Price x quantity
+				$subtotal = ($product->price + $this->input->post('item_qty')[$key]);
+				$this->total = $this->total + $subtotal;
+
+				$paypal_product['items'][] = array(
+					'itm_name' => $product->title,
+					'itm_price' => $product->price,
+					'itm_code' => $item_id,
+					'itm_qty' => $this->input->post(item_qty'')[$key]);
+			}
+
+			//Order Data
+			$order_data = array(
+				'product_id' => $item_id,
+				'user_id' => $this->session->userdata('user_id'),
+				'transaction_id' => 0,
+				'qty'=> $this->input->post('item_qty')[$key],
+				'price'=> $subtotal;
+				'address'=> $this->input->post('address'),
+				'address2' =>$this->input->post('address2'),
+				'phone'=> $this->input->post('phone'),
+				'city'=> $this->input->post('city'),
+				'state'=> $this->input->post('state'));
+
+			//Add order data to database
+			//add_order should be in Cart_model, 
+			//but it contains only one function
+			//so put it into product model.
+			//it there are more functionality, 
+			//create separate cart_model
+			$this->Product_model->add_order($order_data);
+
+		}
 		//Items Data
-		$data = array(
-			'address'=> $this->input->post('address'),
-			'address2' =>$this->input->post('address2'),
-			'price'=> $this->input->post('price'),
-			'phone'=> $this->input->post('phone'),
-			'city'=> $this->input->post('city'),
-			'state'=> $this->input->post('state'));
+		
 
 		//Insert into orders
 		//$this->orders->insert($data);
 		//print_r($data);
 		redirect('products');
 	}
+
 }
 
 ?>
