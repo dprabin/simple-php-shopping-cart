f56b284a9f2ea67103178d9dcd9e841c6f32b665
dprabin <dprabin@yahoo.com>
2015-01-24 14:19:55 +0545

    remove hidden input user id

--- application/views/user.php
+++ application/views/user.php
@@ -12,7 +12,6 @@
   <li>last_active: <?php echo $user->last_active; ?></li>
   </ul>
   <hr>
-  <input type="hidden" name="id" value="<?php echo $user->id; ?>">
   <div class="form-group">
     <label>Old Password*</label>
     <input type="password" class="form-control" name="old_password" placeholder="Enter your current password" />
