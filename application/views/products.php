a32b2e0f0098a6e86e92bda2f8bb07ed64b8ee51
dprabin <dprabin@yahoo.com>
2015-01-24 11:13:13 +0545

    added price per unit

--- application/views/products.php
+++ application/views/products.php
@@ -11,12 +11,12 @@
 <div class="row"><!-- Food items here -->
 <?php foreach($products as $product) : ?>
 	<div class="col-md-4 food">
-		<div class="food-price">Rs.<?php echo $product->price; ?></div>
+		<div class="food-price">Rs.<div class="food-price-per-unit"><?php echo $product->price; ?></div><br />per <?php echo $product->unit; ?></div>
 		<div class="food-image"><a href="<?php echo base_url(); ?>products/details/<?php echo $product->id; ?>"><img class="food-image-img" src="<?php echo base_url(); ?>assets/images/products/<?php echo $product->image; ?>" /></a></div>
 		<div class="food-title"><?php echo $product->title; ?></div>
 		<div class="food-add">
 			<form method="post" action="<?php echo base_url(); ?>cart/add">
-				QTY: <input type="text" class="qty" name="qty" value="1" /> 
+				Quantity: <input type="text" class="qty" name="qty" value="1" /> 
 				<input type="hidden" name="item_number" value="<?php echo $product->id; ?>" />
 				<input type="hidden" name="price" value="<?php echo $product->price; ?>" />
 				<input type="hidden" name="title" value="<?php echo $product->title; ?>" />
