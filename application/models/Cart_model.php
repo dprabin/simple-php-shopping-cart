bdee83c2358bd053bc2a8aa2e3c8ad1c7c1d431b
dprabin <dprabin@yahoo.com>
2015-01-26 20:00:58 +0545

    Changed the lowercase CI_model to CI_Model

--- application/models/Cart_model.php
+++ application/models/Cart_model.php
@@ -1,5 +1,5 @@
 <?php
-class Cart_model extends CI_model{
+class Cart_model extends CI_Model{
 	//add order to database
 	public function add_order($order_data){
 		//Insert order data to db and return
