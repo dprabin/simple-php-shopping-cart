64a7092a9afbf7b5aa5304e358ac0eaa081a56d7
dprabin <dprabin@yahoo.com>
2015-01-26 16:10:43 +0545

    changed the form action

--- application/views/add.php
+++ application/views/add.php
@@ -3,7 +3,7 @@
 <?php endif; ?>
 <?php echo validation_errors('<div class="alert alert-danger">','</div>'); ?>
 
-<form method="post" action="products/edit">
+<form method="post" action="<?php echo base_url(); ?>products/add">
 	<div class="form-group">
 		<label>Product Title</label>
 		<input type="text" name="title" value="<?php echo set_value('title'); ?>" class="form-control" />
@@ -18,7 +18,6 @@
 	</div>
 	<div class="form-group">
 		<label>Product Image</label>
-		<!--img class="food-image-img" src="<?php echo base_url(); ?>assets/images/products/<?php echo $product->image; ?>" width="150px" /-->
 		<input type="text" name="image" value="<?php echo set_value('image'); ?>" class="form-control" />
 	</div>
 	<div class="form-group">
@@ -27,7 +26,7 @@
 	</div>
 	<div class="form-group">
 		<label>Unit of product</label>
-		<input type="text" name="unit" value="<?php echo set_value('unit'); ?>" class="form-control" />
+		<input type="text" name="unit" value="<?php echo set_value('unit','Packet'); ?>" class="form-control" />
 	</div>
 	<div class="form-group">
 		<label>Description</label>
@@ -37,5 +36,5 @@
 		<label>Nutritional Value</label>
 		<textarea name="nutritional_value" rows="5" class="form-control"><?php echo set_value('nutritional_value'); ?></textarea>
 	</div>
-	<button type="submit" class="btn btn-primary" name="submit">Update</button>
+	<button type="submit" class="btn btn-primary" name="submit">Add New Product</button>
 </form>
