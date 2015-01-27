1c8d88c3428d5cb1bb887b8d8540692578251c78
dprabin <dprabin@yahoo.com>
2015-01-27 08:16:04 +0545

    joined get_order function with products and users table

--- application/models/Order_model.php
+++ application/models/Order_model.php
@@ -5,8 +5,10 @@ class Order_model extends CI_Model{
 	//Get the list of orders by status
 	//the status can be (1) pending, (2) delivered, (3) settled, (4) canceled
 	public function get_orders_by_status($status){
-		$this->db->select('*');
-		$this->db->from('orders');
+		$this->db->select('o.*,p.title,u.name');
+		$this->db->from('orders as o');
+		$this->db->join('products as p','o.product_id=p.id','inner');
+		$this->db->join('users as u','o.user_id=u.id','inner');
 		$this->db->where('status',$status);
 		$query = $this->db->get();
 		return $query->results();
@@ -14,15 +16,23 @@ class Order_model extends CI_Model{
 
 	//Get list of all orders in database
 	public function get_orders(){
-		$this->db->select('*');
-		$this->db->from('orders');
+		$this->db->select('o.*,p.title,u.name');
+		$this->db->from('orders as o');
+		$this->db->join('products as p','o.product_id=p.id','inner');
+		$this->db->join('users as u','o.user_id=u.id','inner');
 		$query = $this->db->get();
 		$return $query->results();
 	}
 
 	//Get order by id
 	public function get_order_by_id($id){
-		return false;
+		$this->db->select('o.*,p.title,u.name');
+		$this->db->from('orders as o');
+		$this->db->join('products as p','o.product_id=p.id','inner');
+		$this->db->join('users as u','o.user_id=u.id','inner');
+		$this->db->where('id',$id);
+		$query = $this->db->get();
+		$return $query->row();
 	}
 	
 }
