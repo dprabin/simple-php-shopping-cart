3c7a25be967dcf56782cd88b8bd6cc6dc1cb0c40
dprabin <dprabin@yahoo.com>
2015-01-27 11:18:36 +0545

    added links to other reports by user, product and status

--- application/controllers/reports.php
+++ application/controllers/reports.php
@@ -25,16 +25,43 @@ class Reports extends CI_Controller{
 		}
 	}
 
-	public function orders($status='null'){
-		if(!empty($status){
+	public function orders_by_status($status=null){
+		if(!empty($status)){
 			$status=strtolower($status);
-			if($status!='pending' || $status!='delivered' || $status!='settled' || $status!='canceled' ){
-				$this->session->set_flashdata('action_unsuccessful','The status you supplied is invalid, displaying index page');
+			if($status=='pending' || $status=='delivered' || $status=='settled' || $status=='canceled' ){
+			} else {
+				//$this->session->set_flashdata('action_unsuccessful','The status you supplied is invalid, displaying pending orders');
 				$status='pending';
 			}
 			$this->load->model('Order_model');
 			$data['report_title'] = ucwords($status).' Orders';
-			$data['orders'] = $this->Order_model->get_orders_by($status);
+			$data['orders'] = $this->Order_model->get_orders_by('status',$status);
+			$data['main_content'] = 'reports/report_orders';
+			$this->load->view('layouts/main',$data);
+		} else {
+			$this->session->set_flashdata('action_unsuccessful','You didnt supply the status of order, displaying index page');
+			redirect('reports');
+		}
+	}
+
+	public function orders_by_user($user_id=null){
+		if(!empty($user_id)){
+			$this->load->model('Order_model');
+			$data['report_title'] = 'Ordered by user no: '.$user_id; //get username instead of user id
+			$data['orders'] = $this->Order_model->get_orders_by('user_id',$user_id); //if no data returned, redirect with message
+			$data['main_content'] = 'reports/report_orders';
+			$this->load->view('layouts/main',$data);
+		} else {
+			$this->session->set_flashdata('action_unsuccessful','You didnt supply the status of order, displaying index page');
+			redirect('reports');
+		}
+	}
+
+	public function orders_by_product($product_id=null){
+		if(!empty($product_id)){
+			$this->load->model('Order_model');
+			$data['report_title'] = 'Ordered by user no: '.$product_id; //get username instead of user id
+			$data['orders'] = $this->Order_model->get_orders_by('product_id',$product_id); //if no data returned, redirect with message
 			$data['main_content'] = 'reports/report_orders';
 			$this->load->view('layouts/main',$data);
 		} else {
