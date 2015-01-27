335c474096e459e01373c621dd78efa2702ac8e3
dprabin <dprabin@yahoo.com>
2015-01-27 09:56:57 +0545

    added all_products

--- application/controllers/reports.php
+++ application/controllers/reports.php
@@ -1,11 +1,28 @@
 <?php
 class Reports extends CI_Controller{
 	public function index(){
-		$this->load->model('Order_model');
-		$data['all_orders'] = $this->Order_model->get_orders();
-		$data['pending_orders'] = $this->Order_model->get_orders_by('status','pending'); //status can be pending,delivered,settled,canceled
-		$data['main_content'] = 'reports/report_orders';
-		$this->load->view('layouts/main',$data);
+		if($this->session->userdata('previllege') == 'admin'){
+			$this->load->model('Order_model');
+			$data['report_title'] = 'Pending Orders';
+			$data['orders'] = $this->Order_model->get_orders_by('status','pending'); //status can be pending,delivered,settled,canceled
+			$data['main_content'] = 'reports/report_orders';
+			$this->load->view('layouts/main',$data);
+		} else {
+			$this->session->set_flashdata('action_unsuccessful','You Need Admin Rights to view reports');
+			redirect('products');
+		}
+	}
+	public function all_orders(){
+		if($this->session->userdata('previllege') == 'admin'){
+			$this->load->model('Order_model');
+			$data['report_title'] = 'All Orders';
+			$data['orders'] = $this->Order_model->get_orders();
+			$data['main_content'] = 'reports/report_orders';
+			$this->load->view('layouts/main',$data);
+		} else {
+			$this->session->set_flashdata('action_unsuccessful','You Need Admin Rights to view reports');
+			redirect('products');
+		}
 	}
 }
 ?>
