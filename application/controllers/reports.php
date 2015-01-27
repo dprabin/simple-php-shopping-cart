6921ff19c60aec3df103c2b2f46e024c03db8f11
dprabin <dprabin@yahoo.com>
2015-01-27 08:53:04 +0545

    add pending_orders to view

--- application/controllers/reports.php
+++ application/controllers/reports.php
@@ -1,7 +1,8 @@
 <?php
 class Reports extends CI_Controller{
 	public function index(){
-		$data['orders'] = $this->Order_model->get_orders();
+		$data['all_orders'] = $this->Order_model->get_orders();
+		$data['pending_orders'] = $this->Order_model->get_orders_by('status','pending'); //status can be pending,delivered,settled,canceled
 		$data['main_content'] = 'report_orders';
 		$this->load->view('layouts/main',$data);
 		}
