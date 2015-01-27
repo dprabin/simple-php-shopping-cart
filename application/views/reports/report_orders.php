1258b009121174c79cec1d7206944c487904bf24
dprabin <dprabin@yahoo.com>
2015-01-27 09:10:16 +0545

    Display pending order details

--- application/views/reports/report_orders.php
+++ application/views/reports/report_orders.php
@@ -1,8 +1,17 @@
 <div class="row">
-<?php foreach($all_orders as $order) : ?>
-	<div class="col-md-4 food">
-		<div class="food-price">Rs.<div class="food-price-per-unit"><?php echo $order->price; ?></div><br />per <?php echo $order->unit; ?></div>
-		<div class="food-title"><?php echo $order->title; ?></div>
-	</div>
-<?php endforeach; ?>
+	<table class="table table-striped">
+		<tr><th>id</th><th>Order By</th><th>Phone</th><th>Address</th><th>Order item</th><th>Qty</th><th>Price</th><th>Time</th></tr>
+		<?php foreach($pending_orders as $order) : ?>
+			<tr>
+				<td><?php echo order->id; ?></td>
+				<td><?php echo order->fullname; ?></td>
+				<td><?php echo order->phone; ?></td>
+				<td><?php echo order->fulladdress; ?></td>
+				<td><?php echo order->title; ?></td>
+				<td><?php echo order->qty; ?></td>
+				<td><?php echo order->price; ?></td>
+				<td><?php echo order->timestamp; ?></td>
+			</tr>
+		<?php endforeach; ?>
+	</table>
 </div>
