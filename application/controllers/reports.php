fb28b3b58fefbc16488def73dc37672980fb6b22
dprabin <dprabin@yahoo.com>
2015-01-27 09:29:54 +0545

    manually load Order_model as it is not required everywhere

--- application/controllers/reports.php
+++ application/controllers/reports.php
@@ -1,6 +1,7 @@
 <?php
 class Reports extends CI_Controller{
 	public function index(){
+		$this->load->model('Order_model');
 		$data['all_orders'] = $this->Order_model->get_orders();
 		$data['pending_orders'] = $this->Order_model->get_orders_by('status','pending'); //status can be pending,delivered,settled,canceled
 		$data['main_content'] = 'reports/report_orders';
