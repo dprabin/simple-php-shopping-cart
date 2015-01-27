8824b2f7d5b2a7e90b9b581a9009a646fa1c3d85
dprabin <dprabin@yahoo.com>
2015-01-27 13:29:33 +0545

    List of products view

---
+++ application/views/reports/report_products.php
@@ -0,0 +1,19 @@
+<div class="row">
+	<h3><?php echo $report_title; ?></h3>
+	<table class="table table-striped">
+		<tr><th>id</th><th>Product Name</th><th>Category</th><th>Price</th><th>Unit</th><th>Image</th><th>Description</th><th>Nutritional value</th></tr>
+		<?php foreach($products as $product) : ?>
+			<tr>
+				<td><?php echo $product->id; ?></td>
+				<td><a href="<?php echo base_url(); ?>products/edit/<?php echo $product->id; ?>"><?php echo $product->title; ?></a></td>
+				<td><a href="<?php echo base_url(); ?>reports/products_by_category/<?php echo $product->category_id; ?>"><?php echo $product->name; ?></a></td>
+				<td><?php echo $product->price; ?></td>
+				<td><?php echo $product->unit; ?></td>
+				<td><?php echo $product->image; ?></td>
+				<td><?php echo mb_substr($product->description, 0, strpos($product->description, ' ', 20)); ?> <a href="<?php echo base_url(); ?>products/details/<?php echo $product->id; ?>">read more </a></td>
+				<td><?php echo mb_substr($product->nutritional_value, 0, strpos($product->nutritional_value, ' ', 40)); ?> <a href="<?php echo base_url(); ?>products/details/<?php echo $product->id; ?>">read more </a></td>
+			</tr>
+		<?php endforeach; ?>
+	</table>
+	<br />
+</div>
