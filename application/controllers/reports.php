50e768461a31eb3b25a6dee3cb66a933d7eb4e9f
dprabin <dprabin@yahoo.com>
2015-01-27 10:34:30 +0545

    added function to display order by status

--- application/controllers/reports.php
+++ application/controllers/reports.php
@@ -24,5 +24,23 @@ class Reports extends CI_Controller{
 			redirect('products');
 		}
 	}
+
+	public function orders($status='null'){
+		if(!empty($status){
+			$status=strtolower($status);
+			if($status!='pending' || $status!='delivered' || $status!='settled' || $status!='canceled' ){
+				$this->session->set_flashdata('action_unsuccessful','The status you supplied is invalid, displaying index page');
+				$status='pending';
+			}
+			$this->load->model('Order_model');
+			$data['report_title'] = ucwords($status).' Orders';
+			$data['orders'] = $this->Order_model->get_orders_by($status);
+			$data['main_content'] = 'reports/report_orders';
+			$this->load->view('layouts/main',$data);
+		} else {
+			$this->session->set_flashdata('action_unsuccessful','You didnt supply the status of order, displaying index page');
+			redirect('reports');
+		}
+	}
 }
 ?>
