afb907a24dd62153ee52a092d592085e20276d3f
dprabin <dprabin@yahoo.com>
2015-01-27 08:45:00 +0545

    add prices of orders that are not canceled

--- application/models/Order_model.php
+++ application/models/Order_model.php
@@ -5,12 +5,24 @@ class Order_model extends CI_Model{
 	//the status can be (1) pending, (2) delivered, (3) settled, (4) canceled
 	//Changed this to generic method to get orders by any field with given value
 	//instead of writing several methods
-	public function get_orders_by($field,$value){
+	public function get_orders_by($fieldname,$fieldvalue){
 		$this->db->select('o.*,p.title,u.name');
 		$this->db->from('orders as o');
 		$this->db->join('products as p','o.product_id=p.id','inner');
 		$this->db->join('users as u','o.user_id=u.id','inner');
-		$this->db->where($field,$value);
+		$this->db->where($fieldname,$fieldvalue);
+		$query = $this->db->get();
+		return $query->results();
+	}
+
+	//Get the total amount under any condition
+	public function get_total_by($fieldname){
+		$this->db->select('p.id,p.title,sum(o.price)');
+		$this->db->from('orders as o');
+		$this->db->join('products as p','o.product_id=p.id','inner');
+		$this->db->join('users as u','o.user_id=u.id','inner');
+		$this->db->group_by($fieldname);
+		$this->db->where('o.status !=','canceled'); //not including canceled order
 		$query = $this->db->get();
 		return $query->results();
 	}
