d15c1f8b58452ffd8c1a0b064332a72c705edbee
dprabin <dprabin@yahoo.com>
2015-01-27 12:23:30 +0545

    If no parameter is passed to edit, then redirect to products with message

--- application/controllers/products.php
+++ application/controllers/products.php
@@ -23,46 +23,51 @@ class Products extends CI_Controller{
     }
 
     //Display the detail information of a product
-    public function edit($id){
-        //If logged in as administrator, edit product 
-        //otherwise redirect to details/id
-        if ($this->session->userdata('previllege')=='admin'){
-            $data['product'] = $this->Product_model->get_product_details($id);
-            if($data['product'] || $_POST){
-                if($_POST){
-                    //Validate data
-                    $this->form_validation->set_rules('title','Username', 'trim|required|max_length[200]|min_length[3]');
-                    $this->form_validation->set_rules('category_id','Category', 'trim|required');
-                    $this->form_validation->set_rules('description','Product Description', 'trim|required|min_length[10]');
-                    $this->form_validation->set_rules('nutritional_value','Nutritional Value', 'trim');
-                    $this->form_validation->set_rules('image','Product Image', 'trim|required|max_length[200]|min_length[5]');
-                    $this->form_validation->set_rules('price','Product Price', 'trim|required|max_length[7]|min_length[1]');
-                    $this->form_validation->set_rules('unit','Unit of product', 'trim|required|max_length[25]|min_length[1]');
-                    //Read the product details
-                    $data['product'] = $this->Product_model->get_product_details($this->input->post('product_id'));
-                    if($this->form_validation->run() == FALSE){
+    public function edit($id=null){
+        if(!empty($id)){
+            //If logged in as administrator, edit product 
+            //otherwise redirect to details/id
+            if ($this->session->userdata('previllege')=='admin'){
+                $data['product'] = $this->Product_model->get_product_details($id);
+                if($data['product'] || $_POST){
+                    if($_POST){
+                        //Validate data
+                        $this->form_validation->set_rules('title','Username', 'trim|required|max_length[200]|min_length[3]');
+                        $this->form_validation->set_rules('category_id','Category', 'trim|required');
+                        $this->form_validation->set_rules('description','Product Description', 'trim|required|min_length[10]');
+                        $this->form_validation->set_rules('nutritional_value','Nutritional Value', 'trim');
+                        $this->form_validation->set_rules('image','Product Image', 'trim|required|max_length[200]|min_length[5]');
+                        $this->form_validation->set_rules('price','Product Price', 'trim|required|max_length[7]|min_length[1]');
+                        $this->form_validation->set_rules('unit','Unit of product', 'trim|required|max_length[25]|min_length[1]');
+                        //Read the product details
+                        $data['product'] = $this->Product_model->get_product_details($this->input->post('product_id'));
+                        if($this->form_validation->run() == FALSE){
+                            $data['main_content'] = 'edit';
+                            $this->load->view('layouts/main',$data);
+                        } else {
+                            //Update product
+                            if($this->Product_model->update()){
+                                $this->session->set_flashdata('action_successful','The product '.$this->input->post('title').' is updated');
+                                redirect('products');
+                            } else {
+                                $this->session->set_flashdata('action_unsuccessful','The product '.$this->input->post('title').' is not updated');
+                                redirect('products');
+                            }
+                        }
+                    } else {
                         $data['main_content'] = 'edit';
                         $this->load->view('layouts/main',$data);
-                    } else {
-                        //Update product
-                        if($this->Product_model->update()){
-                            $this->session->set_flashdata('action_successful','The product '.$this->input->post('title').' is updated');
-                            redirect('products');
-                        } else {
-                            $this->session->set_flashdata('action_unsuccessful','The product '.$this->input->post('title').' is not updated');
-                            redirect('products');
-                        }
                     }
                 } else {
-                    $data['main_content'] = 'edit';
-                    $this->load->view('layouts/main',$data);
+                    $this->session->set_flashdata('action_unsuccessful','The product you are trying to edit does not exist');
+                    redirect('products');
                 }
             } else {
-                $this->session->set_flashdata('action_unsuccessful','The product you are trying to edit does not exist');
+                $this->session->set_flashdata('action_unsuccessful','You do not have previllege to Edit products');
                 redirect('products');
             }
         } else {
-            $this->session->set_flashdata('action_unsuccessful','You do not have previllege to Edit products');
+            $this->session->set_flashdata('action_unsuccessful','You cannot edit that item');
             redirect('products');
         }
     }
