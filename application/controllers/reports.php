9058934a1efc61a482a0cd67970b9376aae01c5b
dprabin <dprabin@yahoo.com>
2015-01-27 09:23:30 +0545

    removed unnecessary curly bracket

--- application/controllers/reports.php
+++ application/controllers/reports.php
@@ -5,9 +5,6 @@ class Reports extends CI_Controller{
 		$data['pending_orders'] = $this->Order_model->get_orders_by('status','pending'); //status can be pending,delivered,settled,canceled
 		$data['main_content'] = 'reports/report_orders';
 		$this->load->view('layouts/main',$data);
-		}
 	}
 }
-
-
 ?>
