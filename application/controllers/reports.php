49d38323d50e8f30cc33cf360b2e1604b0986f62
dprabin <dprabin@yahoo.com>
2015-01-27 09:10:43 +0545

    changed view location to reports/report_orders

--- application/controllers/reports.php
+++ application/controllers/reports.php
@@ -3,7 +3,7 @@ class Reports extends CI_Controller{
 	public function index(){
 		$data['all_orders'] = $this->Order_model->get_orders();
 		$data['pending_orders'] = $this->Order_model->get_orders_by('status','pending'); //status can be pending,delivered,settled,canceled
-		$data['main_content'] = 'report_orders';
+		$data['main_content'] = 'reports/report_orders';
 		$this->load->view('layouts/main',$data);
 		}
 	}
