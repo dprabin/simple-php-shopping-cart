1dac1295fec3fcaa20eb5a05145921ab48426d6e
dprabin <dprabin@yahoo.com>
2015-01-27 09:57:41 +0545

    made this view generic so it can be called by many order reports

--- application/views/reports/report_orders.php
+++ application/views/reports/report_orders.php
@@ -1,17 +1,23 @@
 <div class="row">
+	<h3><?php echo $report_title; ?></h3>
 	<table class="table table-striped">
 		<tr><th>id</th><th>Order By</th><th>Phone</th><th>Address</th><th>Order item</th><th>Qty</th><th>Price</th><th>Time</th></tr>
-		<?php foreach($pending_orders as $order) : ?>
+		<?php foreach($orders as $order) : ?>
 			<tr>
-				<td><?php echo order->id; ?></td>
-				<td><?php echo order->fullname; ?></td>
-				<td><?php echo order->phone; ?></td>
-				<td><?php echo order->fulladdress; ?></td>
-				<td><?php echo order->title; ?></td>
-				<td><?php echo order->qty; ?></td>
-				<td><?php echo order->price; ?></td>
-				<td><?php echo order->timestamp; ?></td>
+				<td><?php echo $order->id; ?></td>
+				<td><?php echo $order->fullname; ?></td>
+				<td><?php echo $order->phone; ?></td>
+				<td><?php echo $order->fulladdress; ?></td>
+				<td><?php echo $order->title; ?></td>
+				<td><?php echo $order->qty; ?></td>
+				<td><?php echo $order->price; ?></td>
+				<td><?php echo $order->timestamp; ?></td>
 			</tr>
 		<?php endforeach; ?>
 	</table>
+	<br />
+	<ul>
+		<li><a href="reports/all_orders">All Orders list</a></li>
+		<li><a href="reports">Pending Orders</a></li>
+	</ul>
 </div>
