f62ce81d02a2f0fa00ef75d867f0a609f8aed99a
dprabin <dprabin@yahoo.com>
2015-01-28 09:54:06 +0545

    added links to add, edit and delete category

--- application/views/reports/report_all_categories.php
+++ application/views/reports/report_all_categories.php
@@ -1,14 +1,17 @@
 <div class="row">
 	<h3><?php echo $report_title; ?></h3>
 	<table class="table table-striped">
-		<tr><th>id</th><th>Category</th><th>Number of Products</th></tr>
+		<tr><th>id</th><th>Category</th><th>Number of Products</th><th></th><th></th></tr>
 		<?php foreach($categories as $category) : ?>
 			<tr>
 				<td><?php echo $category->id; ?></td>
-				<td><a href="<?php echo base_url(); ?>products/<?php echo $category->id; ?>"><?php echo $category->name; ?></a></td>
+				<td><a href="<?php echo base_url(); ?>admin/products_by_category/<?php echo $category->name; ?>"><?php echo $category->name; ?></a></td>
 				<td><?php echo $category->product_count; ?></td>
+				<td><a href="<?php echo base_url(); ?>admin/edit_category/<?php echo $category->id; ?>">Edit</a></td>
+				<td><a href="<?php echo base_url(); ?>admin/delete_category/<?php echo $category->id; ?>">Delete</a></td>
 			</tr>
 		<?php endforeach; ?>
 	</table>
 	<br />
+	<p><a href="<?php echo base_url(); ?>admin/add_category">+ Add Category</a></p>
 </div>
