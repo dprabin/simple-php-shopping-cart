9e81dc805de8bf93cf0e2a2eb2c8b7f5b4d36b95
dprabin <dprabin@yahoo.com>
2015-01-28 16:16:03 +0545

    add javascript confirmation on delete

--- application/views/reports/report_products.php
+++ application/views/reports/report_products.php
@@ -1,7 +1,7 @@
 <div class="row">
 	<h3><?php echo $report_title; ?></h3>
 	<table class="table table-striped">
-		<tr><th>id</th><th>Product Name</th><th>Category</th><th>Price</th><th>Unit</th><th>Image</th><th>Description</th><th>Nutritional value</th></tr>
+		<tr><th>id</th><th>Product Name</th><th>Category</th><th>Price</th><th>Unit</th><th>Image</th><th>Description</th><th>Nutritional value</th><th></th></tr>
 		<?php foreach($products as $product) : ?>
 			<tr>
 				<td><?php echo $product->id; ?></td>
@@ -12,6 +12,7 @@
 				<td><?php echo $product->image; ?></td>
 				<td><?php echo mb_substr($product->description, 0, strpos($product->description, ' ', 20)); ?> <a href="<?php echo base_url(); ?>products/details/<?php echo $product->id; ?>" >read more</a></td>
 				<td><?php echo mb_substr($product->nutritional_value, 0, strpos($product->nutritional_value, ' ', 40)); ?> <a href="<?php echo base_url(); ?>products/details/<?php echo $product->id; ?>" >read more</a></td>
+				<td><?=anchor("products/delete/".$product->id,"Delete",array('onclick' => "return confirm('Do you want delete this product?')"))?></td>
 			</tr>
 		<?php endforeach; ?>
 	</table>
