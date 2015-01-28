4238aee213714ba679b183be9ce0cc3999f5079c
dprabin <dprabin@yahoo.com>
2015-01-28 11:48:50 +0545

    autoload Category_model as it is required in every user page

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -109,7 +109,7 @@ $autoload['language'] = array();
 |
 */
 
-$autoload['model'] = array('Product_model','User_model','Cart_model');
+$autoload['model'] = array('Product_model','User_model','Cart_model','Category_model');
 
 
 /* End of file autoload.php */
