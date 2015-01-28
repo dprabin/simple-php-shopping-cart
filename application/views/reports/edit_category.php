22210b36e4c91199c7bde98523e2e6015053c282
dprabin <dprabin@yahoo.com>
2015-01-28 09:55:53 +0545

    Edit existing cateogry

---
+++ application/views/reports/edit_category.php
@@ -0,0 +1,10 @@
+<?php echo validation_errors('<div class="alert alert-danger">','</div>'); ?>
+<h3>New Category</h3>
+<form method="post" action="<?php echo base_url(); ?>users/add_category">
+	<input type="hidden" name="category_id" value="<?php echo $category->id; ?>">
+	<div class="form-group">
+		<label>Category Name*</label>
+		<input type="text" class="form-control" name="category_name" placeholder="Category name" value="<?php echo $category->name; ?>" />
+	</div>
+	<button type="submit" class="btn btn-primary" name="submit">Register</button>
+</form>
