f5acd8ae8f54ef703b729b0ed0fde4a25f6faefe
dprabin <dprabin@yahoo.com>
2015-02-02 00:03:22 +0545

    added information in footer

--- application/views/layouts/includes/footer.php
+++ application/views/layouts/includes/footer.php
@@ -13,6 +13,7 @@
     <div class="row footer">
       <div class="container">
         <p>The Sasto Ramro Food Place &copy; 2015, All Rights Reserved</p>
+        <p>Powered by SQUAD OF TECHNICAL MIND (STM)</p>
       </div>
     </div><!-- /Footer Section -->  
 
